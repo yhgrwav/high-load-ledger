ed4eff4cd70cb09b67a88af8dd63e06fbb9bd906
yeahgor <yeahgorq@gmail.com>
2026-04-24 04:40:24 +0300

    chore: added account repository interface

---
+++ internal/domain/repository/account.go
@@ -0,0 +1,15 @@
+package repository
+
+import (
+	"context"
+	"high-load-ledger/internal/domain/entity"
+
+	"github.com/google/uuid"
+)
+
+type AccountRepository interface {
+	Create(ctx context.Context, acc *entity.Account) error
+	GetForUpdate(ctx context.Context, id uuid.UUID) (*entity.Account, error)
+	GetByID(ctx context.Context, id uuid.UUID) (*entity.Account, error)
+	UpdateBalance(ctx context.Context, id uuid.UUID, newAmount int64) error
+}
