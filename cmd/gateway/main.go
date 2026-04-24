a8b3a58b68ca81ee37fb7a969c76122380116072
yeahgor <yeahgorq@gmail.com>
2026-04-24 04:42:00 +0300

    chore: added context, config initialization, logger initialization

--- cmd/gateway/main.go
+++ cmd/gateway/main.go
@@ -1,7 +1,19 @@
 package main
 
-import "log"
+import (
+	"context"
+	"high-load-ledger/internal/infra/logger"
+	"log"
+)
 
 func main() {
 	log.Println("Ledger started...")
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	cfg := config.NewConfig()
+
+	l := logger.New(cfg.logLevel, cfg.addSource, cfg.IsJSON)
+
 }
