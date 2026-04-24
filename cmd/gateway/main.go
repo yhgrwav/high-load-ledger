4476f8a7a74bf60b2758a15fc01688ebddbda564
yeahgor <yeahgorq@gmail.com>
2026-04-24 09:02:21 +0300

    chore: added db ping, defer functions, repo initialization, config initialization, changed context type, added config fields into initializators

--- cmd/gateway/main.go
+++ cmd/gateway/main.go
@@ -2,18 +2,42 @@ package main
 
 import (
 	"context"
+	"high-load-ledger/internal/config"
 	"high-load-ledger/internal/infra/logger"
+	"high-load-ledger/internal/repository/postgres"
 	"log"
+	"os"
+	"time"
+
+	"github.com/jackc/pgx/v5/pgxpool"
 )
 
 func main() {
 	log.Println("Ledger started...")
 
-	ctx, cancel := context.WithCancel(context.Background())
+	cfg, err := config.NewConfig()
+	if err != nil {
+		log.Fatal(err) // os.Exit(1) w no defer
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
 
-	cfg := config.NewConfig()
+	lgr := logger.New(cfg.LogLevel, cfg.AddSource, cfg.IsJSON)
 
-	l := logger.New(cfg.logLevel, cfg.addSource, cfg.IsJSON)
+	pool, err := pgxpool.New(ctx, cfg.DSN)
+	if err != nil {
+		lgr.Error("Unable to create connection pool", "error", err)
+		os.Exit(1)
+	}
+
+	if err := pool.Ping(ctx); err != nil {
+		lgr.Error("Database is unreachable", "error", err)
+		os.Exit(1)
+	}
+
+	defer pool.Close()
+
+	repo := postgres.NewConnectionPool(pool, lgr)
 
 }
