d36f79142264cdb5357e1018b4a0c5d1be71bc49
yeahgor <yeahgorq@gmail.com>
2026-04-25 04:08:47 +0300

    chore: removed UpdatedAt, now its updates itself by the trigger in db

--- internal/domain/entity/account.go
+++ internal/domain/entity/account.go
@@ -1,14 +1,11 @@
 package entity
 
 import (
-	"time"
-
 	"github.com/google/uuid"
 )
 
 type Account struct {
-	ID        uuid.UUID
-	Balance   int64
-	Currency  Currency
-	UpdatedAt time.Time
+	ID       uuid.UUID
+	Balance  int64
+	Currency Currency
 }
