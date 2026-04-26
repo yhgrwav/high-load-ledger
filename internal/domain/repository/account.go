f11d954216e162777afb06ce309c830d9df28ab3
yeahgor <yeahgorq@gmail.com>
2026-04-26 11:48:37 +0300

    chore: added tx entity

--- internal/domain/repository/account.go
+++ internal/domain/repository/account.go
@@ -9,9 +9,9 @@ import (
 
 type AccountRepository interface {
 	CreateAccount(ctx context.Context, acc *entity.Account) error
-	GetForUpdate(ctx context.Context, id uuid.UUID) (*entity.Account, error)
+	GetForUpdate(ctx context.Context, tx entity.CustomTx, id uuid.UUID) (*entity.Account, error)
 	GetByID(ctx context.Context, id uuid.UUID) (*entity.Account, error)
-	UpdateBalance(ctx context.Context, id uuid.UUID, newAmount int64) error
+	UpdateBalance(ctx context.Context, tx entity.CustomTx, id uuid.UUID, newAmount int64) error
 	BeginAccountTx(ctx context.Context) (entity.CustomTx, error)
 	CommitAccountTx(ctx context.Context, tx entity.CustomTx) error
 	RollbackAccountTx(ctx context.Context, tx entity.CustomTx) error
