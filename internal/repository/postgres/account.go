35d27a1daf77cb8b1d2e8210e54aa394a24625fd
yeahgor <yeahgorq@gmail.com>
2026-04-28 18:18:31 +0300

    chore: removed castTx into a common file

--- internal/repository/postgres/account.go
+++ internal/repository/postgres/account.go
@@ -102,18 +102,3 @@ func (db *Repository) RollbackAccountTx(ctx context.Context, tx entity.CustomTx)
 	}
 	return t.Rollback(ctx)
 }
-
-// castTx - локальная вспомогательный метод, который будет типизировать тип any в pgx.Tx (конкретно в этой реализации)
-// и будет возвращать сущность, с которой будет удобно работать внутри других методов, а также этот метод поможет избежать
-// дублирования кода, что способствует реализации принципа DRY
-func (db *Repository) castTx(ctx context.Context, tx entity.CustomTx) (pgx.Tx, error) {
-	if tx == nil {
-		db.logger.ErrorContext(ctx, "db: failed to cast tx")
-		return nil, entity.ErrInvalidTxType
-	}
-	pgTx, ok := tx.(pgx.Tx)
-	if !ok {
-		return nil, entity.ErrInvalidTxType
-	}
-	return pgTx, nil
-}
