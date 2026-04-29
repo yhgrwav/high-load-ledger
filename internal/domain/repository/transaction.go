d18c306ed16745037f0018aedac52f28b90aef71
yeahgor <yeahgorq@gmail.com>
2026-04-29 07:41:23 +0300

    fix: no more pointer in customTx

--- internal/domain/repository/transaction.go
+++ internal/domain/repository/transaction.go
@@ -8,7 +8,7 @@ import (
 )
 
 type TransactionRepository interface {
-	CreateTransaction(ctx context.Context, tx *entity.CustomTx, tr *entity.Transaction) error
+	CreateTransaction(ctx context.Context, tx entity.CustomTx, tr *entity.Transaction) error
 	GetTransactionByID(ctx context.Context, id uuid.UUID) (*entity.Transaction, error)
 	CheckIdempotencyKey(ctx context.Context, key uuid.UUID) (*entity.Transaction, error)
 	UpdateStatus(ctx context.Context, tx entity.CustomTx, id uuid.UUID, status entity.TransactionStatus) error
