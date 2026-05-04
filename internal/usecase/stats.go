029979fc52c47d8b517b0bcb545c60e3a7e48cd1
yeahgor <yeahgorq@gmail.com>
2026-05-05 02:36:21 +0300

    feat: added stats usecase struct, constructor and methods

--- internal/usecase/stats.go
+++ internal/usecase/stats.go
@@ -1,1 +1,31 @@
 package usecase
+
+import (
+	"context"
+	"log/slog"
+
+	"github.com/google/uuid"
+
+	"high-load-ledger/internal/domain/entity"
+	"high-load-ledger/internal/domain/repository"
+)
+
+type StatsUseCase struct {
+	repo   repository.TransactionRepository
+	logger *slog.Logger
+}
+
+func NewStatsUseCase(repo repository.TransactionRepository, logger *slog.Logger) *StatsUseCase {
+	return &StatsUseCase{
+		repo:   repo,
+		logger: logger,
+	}
+}
+
+func (s *StatsUseCase) GetTransaction(ctx context.Context, id uuid.UUID) (*entity.Transaction, error) {
+	return s.repo.GetTransactionByID(ctx, id)
+}
+
+func (s *StatsUseCase) UpdateTransactionStatus(ctx context.Context, id uuid.UUID, tx entity.CustomTx, status entity.TransactionStatus) error {
+	return s.repo.UpdateStatus(ctx, tx, id, status)
+}
