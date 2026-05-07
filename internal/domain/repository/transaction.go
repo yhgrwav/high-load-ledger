9bf0e52d0126d69c0d54a1cb19fa9e8df64a74c9
yeahgor <yeahgorq@gmail.com>
2026-05-07 16:45:51 +0300

    chore: added GetTransactionByIdempotencyKey method

--- internal/domain/repository/transaction.go
+++ internal/domain/repository/transaction.go
@@ -11,6 +11,7 @@ type TransactionRepository interface {
 	CreateTransaction(ctx context.Context, tx entity.CustomTx, tr *entity.Transaction) error
 	GetTransactionByID(ctx context.Context, id uuid.UUID) (*entity.Transaction, error)
 	CheckIdempotencyKey(ctx context.Context, key uuid.UUID) (*entity.Transaction, error)
+	GetTransactionByIdempotencyKey(ctx context.Context, tx entity.CustomTx, key uuid.UUID) (*entity.Transaction, error)
 	UpdateStatus(ctx context.Context, tx entity.CustomTx, id uuid.UUID, status entity.TransactionStatus) error
 
 	BeginTx(ctx context.Context) (entity.CustomTx, error)
