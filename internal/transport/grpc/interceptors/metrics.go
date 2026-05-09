f147d330a19bb208970aac1b1a065bd0185cd10e
yeahgor <yeahgorq@gmail.com>
2026-05-09 07:29:54 +0300

    feat: my first grpc interceptor no way mlg

--- internal/transport/grpc/interceptors/metrics.go
+++ internal/transport/grpc/interceptors/metrics.go
@@ -1,21 +1,32 @@
 package interceptors
 
-import "github.com/prometheus/client_golang/prometheus"
+import (
+	"context"
+	"high-load-ledger/internal/infra/telemetry"
+	"time"
 
-var (
-	requestsCounter = prometheus.NewCounterVec(prometheus.CounterOpts{
-		Namespace: "leger",                 // prefix
-		Subsystem: "grpc",                  // api type
-		Name:      "total_requests",        // name
-		Help:      "grpc requests counter", // description
-	}, []string{"service", "method,", "code"}, // labels
-	)
-
-	requestDuration = prometheus.NewHistogramVec(prometheus.HistogramOpts{
-		Namespace: "ledger",
-		Subsystem: "grpc",
-		Name:      "request_duration",
-		Help:      "duration of grpc requests",
-		Buckets:   []float64{0.01, 0.05, 0.1, 0.25, 0.5, 1, 2, 5},
-	}, []string{"method", "code"})
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/status"
 )
+
+func UnaryMetricsInterceptor(m telemetry.Metrics) grpc.UnaryServerInterceptor {
+	return func(ctx context.Context, req interface{}, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (interface{}, error) {
+		start := time.Now()
+
+		resp, err := handler(ctx, req)
+		if err != nil {
+			return nil, err
+		}
+
+		result := float64(time.Since(start).Seconds())
+
+		st, _ := status.FromError(err)
+
+		code := st.Code().String()
+
+		m.ObserveResponseTime(info.FullMethod, code, result)
+		m.ObserveTotalRequests(info.FullMethod, code)
+
+		return resp, err
+	}
+}
