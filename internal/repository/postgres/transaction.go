d25fcebdb33e6d769634dffb708311fa0553f0f1
yeahgor <yeahgorq@gmail.com>
2026-05-19 11:00:46 +0300

    chore: removed status from createTransaction, GetTransactionByID and CheckIdempotencyKey methods, refactored error handling, removed UpdateStatus method

--- internal/repository/postgres/transaction.go
+++ internal/repository/postgres/transaction.go
@@ -2,6 +2,7 @@ package postgres
 
 import (
 	"context"
+	"errors"
 	"fmt"
 	"high-load-ledger/internal/domain/entity"
 
@@ -10,14 +11,14 @@ import (
 )
 
 func (db *Repository) CreateTransaction(ctx context.Context, tx entity.CustomTx, tr *entity.Transaction) error {
-	query := `INSERT INTO ledger.transactions (id, user_from, user_to, currency, amount, idempotency_key, status, created_at)
-				  VALUES ($1, $2, $3, $4, $5, $6, $7, $8)`
+	query := `INSERT INTO ledger.transactions (id, user_from, user_to, currency, amount, idempotency_key, created_at)
+				  VALUES ($1, $2, $3, $4, $5, $6, $7)`
 
 	t, err := db.castTx(ctx, tx)
 	if err != nil {
 		return err
 	}
-	_, err = t.Exec(ctx, query, tr.ID, tr.FromAccountID, tr.ToAccountID, tr.Currency, tr.Amount, tr.IdempotencyKey, tr.Status, tr.CreatedAt)
+	_, err = t.Exec(ctx, query, tr.ID, tr.FromAccountID, tr.ToAccountID, tr.Currency, tr.Amount, tr.IdempotencyKey, tr.CreatedAt)
 	if err != nil {
 		db.logger.ErrorContext(ctx, "db: create transaction error", "err", err)
 		return err
@@ -26,15 +27,15 @@ func (db *Repository) CreateTransaction(ctx context.Context, tx entity.CustomTx,
 }
 
 func (db *Repository) GetTransactionByID(ctx context.Context, id uuid.UUID) (*entity.Transaction, error) {
-	query := `SELECT id, user_from, user_to, currency, amount, idempotency_key, status, created_at
+	query := `SELECT id, user_from, user_to, currency, amount, idempotency_key, created_at
               FROM ledger.transactions 
               WHERE id = $1`
 
 	var tr entity.Transaction
 
-	err := db.pool.QueryRow(ctx, query, id).Scan(&tr.ID, &tr.FromAccountID, &tr.ToAccountID, &tr.Currency, &tr.Amount, &tr.IdempotencyKey, &tr.Status, &tr.CreatedAt)
+	err := db.pool.QueryRow(ctx, query, id).Scan(&tr.ID, &tr.FromAccountID, &tr.ToAccountID, &tr.Currency, &tr.Amount, &tr.IdempotencyKey, &tr.CreatedAt)
 	if err != nil {
-		if err == pgx.ErrNoRows {
+		if errors.Is(err, pgx.ErrNoRows) {
 			db.logger.ErrorContext(ctx, "db: transaction_id not found", "err", err)
 			return nil, entity.ErrTransactionNotFound
 		}
@@ -45,36 +46,21 @@ func (db *Repository) GetTransactionByID(ctx context.Context, id uuid.UUID) (*en
 }
 
 func (db *Repository) CheckIdempotencyKey(ctx context.Context, key uuid.UUID) (*entity.Transaction, error) {
-	query := `SELECT id, user_from, user_to, currency, amount, idempotency_key, status, created_at
+	query := `SELECT id, user_from, user_to, currency, amount, idempotency_key, created_at
               FROM ledger.transactions
               WHERE idempotency_key = $1`
 
 	var tr entity.Transaction
 
-	err := db.pool.QueryRow(ctx, query, key).Scan(&tr.ID, &tr.FromAccountID, &tr.ToAccountID, &tr.Currency, &tr.Amount, &tr.IdempotencyKey, &tr.Status, &tr.CreatedAt)
+	err := db.pool.QueryRow(ctx, query, key).Scan(&tr.ID, &tr.FromAccountID, &tr.ToAccountID, &tr.Currency, &tr.Amount, &tr.IdempotencyKey, &tr.CreatedAt)
 	if err != nil {
-		if err == pgx.ErrNoRows {
+		if errors.Is(err, pgx.ErrNoRows) {
 			return nil, entity.ErrTransactionNotFound
 		}
 		db.logger.ErrorContext(ctx, "db: get transaction error", "err", err)
 		return nil, fmt.Errorf("db: check idempotency key error: %w", err)
 	}
 	return &tr, nil
-}
-
-func (db *Repository) UpdateStatus(ctx context.Context, tx entity.CustomTx, id uuid.UUID, status entity.TransactionStatus) error {
-	t, err := db.castTx(ctx, tx)
-	if err != nil {
-		return err
-	}
-
-	query := `UPDATE ledger.transactions SET status = $1 WHERE id = $2`
-	_, err = t.Exec(ctx, query, status, id)
-	if err != nil {
-		db.logger.ErrorContext(ctx, "db: update transaction error", "err", err)
-		return fmt.Errorf("db: update transaction error: %w", err)
-	}
-	return nil
 }
 
 func (db *Repository) BeginTx(ctx context.Context) (entity.CustomTx, error) {
