46ed5a4446707c9e9b31116076768fe91f715186
yeahgor <yeahgorq@gmail.com>
2026-05-19 11:01:34 +0300

    refactor: removed status enum, removed status from Transaction struct

--- internal/domain/entity/transcation.go
+++ internal/domain/entity/transcation.go
@@ -6,15 +6,6 @@ import (
 	"github.com/google/uuid"
 )
 
-type TransactionStatus int16
-
-const (
-	STATUS_UNKNOWN TransactionStatus = iota
-	STATUS_PENDING
-	STATUS_COMPLETED
-	STATUS_FAIILED
-)
-
 type Transaction struct {
 	ID             uuid.UUID
 	FromAccountID  uuid.UUID
@@ -22,7 +13,6 @@ type Transaction struct {
 	Currency       Currency
 	Amount         int64
 	IdempotencyKey uuid.UUID
-	Status         TransactionStatus
 	CreatedAt      time.Time
 }
 
