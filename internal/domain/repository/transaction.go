3aeb1b5f3db00d30be7d69049fb2dcb8b3a2ac93
yeahgor <yeahgorq@gmail.com>
2026-05-19 10:58:43 +0300

    chore: removed UpdateStatus method

--- internal/domain/repository/transaction.go
+++ internal/domain/repository/transaction.go
@@ -11,7 +11,6 @@ type TransactionRepository interface {
 	CreateTransaction(ctx context.Context, tx entity.CustomTx, tr *entity.Transaction) error
 	GetTransactionByID(ctx context.Context, id uuid.UUID) (*entity.Transaction, error)
 	CheckIdempotencyKey(ctx context.Context, key uuid.UUID) (*entity.Transaction, error)
-	UpdateStatus(ctx context.Context, tx entity.CustomTx, id uuid.UUID, status entity.TransactionStatus) error
 
 	BeginTx(ctx context.Context) (entity.CustomTx, error)
 	CommitTx(ctx context.Context, tx entity.CustomTx) error
