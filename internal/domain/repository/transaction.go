6698d42ff551ea5fa4aba77280bd50539cacb28d
yeahgor <yeahgorq@gmail.com>
2026-06-02 06:01:01 +0300

    refactor: CheckIdempotencyKey now returns only uuid

--- internal/domain/repository/transaction.go
+++ internal/domain/repository/transaction.go
@@ -10,7 +10,7 @@ import (
 type TransactionRepository interface {
 	CreateTransaction(ctx context.Context, tx entity.CustomTx, tr *entity.Transaction) error
 	GetTransactionByID(ctx context.Context, id uuid.UUID) (*entity.Transaction, error)
-	CheckIdempotencyKey(ctx context.Context, key uuid.UUID) (*entity.Transaction, error)
+	CheckIdempotencyKey(ctx context.Context, key uuid.UUID) (uuid.UUID, error)
 
 	BeginTx(ctx context.Context) (entity.CustomTx, error)
 	CommitTx(ctx context.Context, tx entity.CustomTx) error
