8a735411416b650fe122b28f5bcaa53cf1f8b84b
yeahgor <yeahgorq@gmail.com>
2026-06-05 12:11:29 +0300

    feat: implemented stats handler

--- internal/transport/grpc/stats.go
+++ internal/transport/grpc/stats.go
@@ -2,12 +2,14 @@ package grpc
 
 import (
 	"context"
+	"errors"
 
 	"github.com/google/uuid"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
 
 	ledger "high-load-ledger/gen/go"
+	"high-load-ledger/internal/domain/entity"
 )
 
 func (h *Handler) GetTransaction(ctx context.Context, req *ledger.GetTransactionRequest) (*ledger.GetTransactionResponse, error) {
@@ -17,6 +19,20 @@ func (h *Handler) GetTransaction(ctx context.Context, req *ledger.GetTransaction
 		return nil, status.Error(codes.InvalidArgument, "invalid transaction_id")
 	}
 
-	h.logger.InfoContext(ctx, "GetTransaction stub called", "tx_id", txID)
-	return nil, status.Error(codes.Unimplemented, "not implemented yet")
+	tx, err := h.statsUC.GetTransaction(ctx, txID)
+	if err != nil {
+		if errors.Is(err, entity.ErrTransactionNotFound) {
+			return nil, status.Error(codes.NotFound, "transaction not found")
+		}
+		h.logger.ErrorContext(ctx, "get transaction failed", "error", err, "transaction_id", txID)
+		return nil, status.Error(codes.Internal, "get transaction failed")
+	}
+
+	return &ledger.GetTransactionResponse{
+		Id:       tx.ID[:],
+		UserFrom: tx.FromAccountID[:],
+		UserTo:   tx.ToAccountID[:],
+		Amount:   tx.Amount,
+		Currency: ledger.Currency(tx.Currency),
+	}, nil
 }
