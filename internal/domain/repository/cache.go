2b0d864a422a70ebdb7f915653281ce9f02bea0e
yeahgor <yeahgorq@gmail.com>
2026-06-05 11:01:51 +0300

    chore: added setter and getter for transaction

--- internal/domain/repository/cache.go
+++ internal/domain/repository/cache.go
@@ -5,6 +5,8 @@ import (
 	"time"
 
 	"github.com/google/uuid"
+
+	"high-load-ledger/internal/domain/entity"
 )
 
 type CacheRepository interface {
@@ -14,4 +16,7 @@ type CacheRepository interface {
 	SetBalance(ctx context.Context, accountID uuid.UUID, amount int64, ttl time.Duration) error
 	GetBalance(ctx context.Context, accountID uuid.UUID) (int64, error)
 	DeleteBalance(ctx context.Context, accountID uuid.UUID) error
+
+	SetTransaction(ctx context.Context, tx *entity.Transaction, ttl time.Duration) error
+	GetTransaction(ctx context.Context, id uuid.UUID) (*entity.Transaction, error)
 }
