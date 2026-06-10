c65197fbbca1f1fec9dd39ae20868c83468cb5d2
yeahgor <yeahgorq@gmail.com>
2026-06-10 09:18:22 +0300

    fix: idempotency methods naming

--- internal/domain/repository/cache.go
+++ internal/domain/repository/cache.go
@@ -9,9 +9,9 @@ import (
 )
 
 type CacheRepository interface {
-	TryGetIdempotencyKey(ctx context.Context, key uuid.UUID, ttl time.Duration) (bool, error)
-	SetIdempotencyKey(ctx context.Context, key uuid.UUID, value []byte, ttl time.Duration) error
-	GetIdempotencyKey(ctx context.Context, key uuid.UUID) (entity.IdempotencyEntry, error)
+	LockIdempotencyKey(ctx context.Context, key uuid.UUID, ttl time.Duration) (acquired bool, err error)
+	GetIdempotencyKey(ctx context.Context, key uuid.UUID) ([]byte, error)
+	SaveIdempotencyResponse(ctx context.Context, key uuid.UUID, response []byte, ttl time.Duration) error
 	DeleteIdempotencyKey(ctx context.Context, key uuid.UUID) error
 
 	SetBalance(ctx context.Context, accountID uuid.UUID, amount int64, ttl time.Duration) error
