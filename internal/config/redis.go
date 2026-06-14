d5264d6f1e3642ce7f0a94ec38a36f93cc2057fa
yeahgor <yeahgorq@gmail.com>
2026-06-15 00:51:26 +0300

    fix: renamed vars

--- internal/config/redis.go
+++ internal/config/redis.go
@@ -8,16 +8,14 @@ import (
 )
 
 type Redis struct {
-	Host                string        `env:"REDIS_HOST"`
-	Port                string        `env:"REDIS_PORT" envDefault:"6379"`
-	Password            string        `env:"REDIS_PASSWORD"`
-	DB                  int           `env:"REDIS_DB" envDefault:"0"`
-	PoolSize            int           `env:"REDIS_POOL_SIZE" envDefault:"500"`
-	MinIdleConns        int           `env:"REDIS_MIN_IDLE_CONNS" envDefault:"50"`
-	PoolTimeout         time.Duration `env:"REDIS_POOL_TIMEOUT" envDefault:"4s"`
-	IdempotencyPoolSize int           `env:"REDIS_IDEMPOTENCY_POOL_SIZE" envDefault:"1024"`
-	IdempotencyMinIdle  int           `env:"REDIS_IDEMPOTENCY_MIN_IDLE" envDefault:"64"`
-	TransactionTTL      time.Duration `env:"REDIS_TRANSACTION_TTL" envDefault:"67m"`
+	Host           string        `env:"REDIS_HOST"`
+	Port           string        `env:"REDIS_PORT" envDefault:"6379"`
+	Password       string        `env:"REDIS_PASSWORD"`
+	DB             int           `env:"REDIS_DB" envDefault:"0"`
+	PoolSize       int           `env:"REDIS_POOL_SIZE" envDefault:"1500"`
+	MinIdleConns   int           `env:"REDIS_MIN_IDLE_CONNS" envDefault:"200"`
+	PoolTimeout    time.Duration `env:"REDIS_POOL_TIMEOUT" envDefault:"4s"`
+	TransactionTTL time.Duration `env:"REDIS_TRANSACTION_TTL" envDefault:"67m"`
 }
 
 func LoadRedis() (Redis, error) {
